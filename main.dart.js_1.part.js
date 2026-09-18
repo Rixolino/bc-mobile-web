d0a5a1470aa0239ac362292f6a037992a519f817
Simone Risola <elrisolix.erx@outlook.com>
2026-09-18 15:52:04 +0200

    ottimizzazione sistema TTS con formattazione orari localizzata, implementazione auto-annuncio per variazioni di ritardo e rimozione dei pulsanti di lettura manuale dai tabelloni

--- main.dart.js_1.part.js
+++ main.dart.js_1.part.js
@@ -1,7 +1,7 @@
 ((a,b)=>{a[b]=a[b]||{}})(self,"$__dart_deferred_initializers__")
 $__dart_deferred_initializers__.current=function(a,b,c,$){var A,B={
-bQZ(){return new A.uI(null)}}
+bQM(){return new A.uG(null)}}
 A=c[0]
 B=a.updateHolder(c[3],B)
 var z=a.updateTypes([])};
-(a=>{a["Vo/Mq+Nwb8fVWGtKOARaOBQbzKY="]=a.current})($__dart_deferred_initializers__);
+(a=>{a["14YXW+oHZVS5HmAiXemOgNWiYa0="]=a.current})($__dart_deferred_initializers__);
