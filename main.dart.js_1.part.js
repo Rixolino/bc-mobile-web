44f6d61060a5f5f077f00279cef43950ec401fea
Simone Risola <elrisolix.erx@outlook.com>
2026-09-18 18:54:51 +0200

    miglioramento sistema auto-annuncio TTS con ottimizzazione del calcolo dell'orario effettivo e gestione prioritaria tra ritardo e orario stimato

--- main.dart.js_1.part.js
+++ main.dart.js_1.part.js
@@ -1,7 +1,7 @@
 ((a,b)=>{a[b]=a[b]||{}})(self,"$__dart_deferred_initializers__")
 $__dart_deferred_initializers__.current=function(a,b,c,$){var A,B={
-bQM(){return new A.uG(null)}}
+bQT(){return new A.uI(null)}}
 A=c[0]
 B=a.updateHolder(c[3],B)
 var z=a.updateTypes([])};
-(a=>{a["14YXW+oHZVS5HmAiXemOgNWiYa0="]=a.current})($__dart_deferred_initializers__);
+(a=>{a["dtx7NfiS1XySaEqLkxCHVOrMTG8="]=a.current})($__dart_deferred_initializers__);
