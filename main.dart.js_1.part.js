9278ccf91f1c56494bd6fa50d4c26771129d540c
Simone Risola <elrisolix.erx@outlook.com>
2026-09-18 15:35:50 +0200

    miglioramento sistema TTS con localizzazione delle stringhe, gestione dinamica delle voci per lingua, espansione mappatura categorie treni e implementazione auto-annuncio per nuovi treni in tabellone

--- main.dart.js_1.part.js
+++ main.dart.js_1.part.js
@@ -1,7 +1,7 @@
 ((a,b)=>{a[b]=a[b]||{}})(self,"$__dart_deferred_initializers__")
 $__dart_deferred_initializers__.current=function(a,b,c,$){var A,B={
-bQE(){return new A.uH(null)}}
+bQZ(){return new A.uI(null)}}
 A=c[0]
 B=a.updateHolder(c[3],B)
 var z=a.updateTypes([])};
-(a=>{a["8gYTO+oqTyov65K8LBJ5EuPvyz8="]=a.current})($__dart_deferred_initializers__);
+(a=>{a["Vo/Mq+Nwb8fVWGtKOARaOBQbzKY="]=a.current})($__dart_deferred_initializers__);
