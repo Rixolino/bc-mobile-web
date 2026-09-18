168370e1066aac22f0e9c0ab0c1d310a1c7c05e1
Simone Risola <elrisolix.erx@outlook.com>
2026-09-18 12:47:30 +0200

    implementazione impostazioni di accessibilità per lo zoom dei testi, aggiunta di badge colorati per i ritardi nei dettagli treni e aggiornamento localizzazioni

--- main.dart.js_1.part.js
+++ main.dart.js_1.part.js
@@ -1,7 +1,7 @@
 ((a,b)=>{a[b]=a[b]||{}})(self,"$__dart_deferred_initializers__")
 $__dart_deferred_initializers__.current=function(a,b,c,$){var A,B={
-bLO(){return new A.ub(null)}}
+bM5(){return new A.ue(null)}}
 A=c[0]
 B=a.updateHolder(c[3],B)
 var z=a.updateTypes([])};
-(a=>{a["2hbH2w5Li5nwWRmgV63qDavuzQI="]=a.current})($__dart_deferred_initializers__);
+(a=>{a["AMmurSZzB+krdbc0h6DkZQCudRI="]=a.current})($__dart_deferred_initializers__);
