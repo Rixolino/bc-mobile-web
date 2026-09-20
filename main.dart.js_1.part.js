596651c025893a655a6abe919493568ead105ef2
Simone Risola <elrisolix.erx@outlook.com>
2026-09-20 18:06:15 +0200

    fix: uniforma tabellone e Station Details, correggi tasto Aggiorna nei dettagli treno

--- main.dart.js_1.part.js
+++ main.dart.js_1.part.js
@@ -1,7 +1,7 @@
 ((a,b)=>{a[b]=a[b]||{}})(self,"$__dart_deferred_initializers__")
 $__dart_deferred_initializers__.current=function(a,b,c,$){var A,B={
-bQD(){return new A.uI(null)}}
+bQM(){return new A.uI(null)}}
 A=c[0]
 B=a.updateHolder(c[3],B)
 var z=a.updateTypes([])};
-(a=>{a["E02PjaCFGZTT2aCMTFkmtt92mgc="]=a.current})($__dart_deferred_initializers__);
+(a=>{a["327GD3/pUqEKmiZuX9lDNus+seU="]=a.current})($__dart_deferred_initializers__);
