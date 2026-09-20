8220f01b7380d4044ca9c453a4d78822c8a043fa
Simone Risola <elrisolix.erx@outlook.com>
2026-09-20 17:31:25 +0200

    abbellimento tabellone

--- main.dart.js_1.part.js
+++ main.dart.js_1.part.js
@@ -1,7 +1,7 @@
 ((a,b)=>{a[b]=a[b]||{}})(self,"$__dart_deferred_initializers__")
 $__dart_deferred_initializers__.current=function(a,b,c,$){var A,B={
-bQT(){return new A.uI(null)}}
+bQD(){return new A.uI(null)}}
 A=c[0]
 B=a.updateHolder(c[3],B)
 var z=a.updateTypes([])};
-(a=>{a["dtx7NfiS1XySaEqLkxCHVOrMTG8="]=a.current})($__dart_deferred_initializers__);
+(a=>{a["E02PjaCFGZTT2aCMTFkmtt92mgc="]=a.current})($__dart_deferred_initializers__);
