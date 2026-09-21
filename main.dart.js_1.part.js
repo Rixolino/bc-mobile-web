c139519f0711d1e5342fd27b26457f5ec2638378
Simone Risola <elrisolix.erx@outlook.com>
2026-09-21 14:57:59 +0200

    fix definitivo sull'estrazione ritardo da stazioni successive

--- main.dart.js_1.part.js
+++ main.dart.js_1.part.js
@@ -1,7 +1,7 @@
 ((a,b)=>{a[b]=a[b]||{}})(self,"$__dart_deferred_initializers__")
 $__dart_deferred_initializers__.current=function(a,b,c,$){var A,B={
-bQT(){return new A.uJ(null)}}
+bQW(){return new A.uJ(null)}}
 A=c[0]
 B=a.updateHolder(c[3],B)
 var z=a.updateTypes([])};
-(a=>{a["2TFHPDTG8mkaMgbU5xLu0w/Q/+Y="]=a.current})($__dart_deferred_initializers__);
+(a=>{a["SH+gK56APXewbQr7zY569GvdnAA="]=a.current})($__dart_deferred_initializers__);
