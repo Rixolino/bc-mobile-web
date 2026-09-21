74c4aae4bf103bf308b71fc465cd73e506ea6785
Simone Risola <elrisolix.erx@outlook.com>
2026-09-21 13:22:50 +0200

    implementazione sistema di presenza live per i dettagli treno regionali e nazionali

--- main.dart.js_1.part.js
+++ main.dart.js_1.part.js
@@ -1,7 +1,7 @@
 ((a,b)=>{a[b]=a[b]||{}})(self,"$__dart_deferred_initializers__")
 $__dart_deferred_initializers__.current=function(a,b,c,$){var A,B={
-bQM(){return new A.uI(null)}}
+bQT(){return new A.uJ(null)}}
 A=c[0]
 B=a.updateHolder(c[3],B)
 var z=a.updateTypes([])};
-(a=>{a["327GD3/pUqEKmiZuX9lDNus+seU="]=a.current})($__dart_deferred_initializers__);
+(a=>{a["2TFHPDTG8mkaMgbU5xLu0w/Q/+Y="]=a.current})($__dart_deferred_initializers__);
