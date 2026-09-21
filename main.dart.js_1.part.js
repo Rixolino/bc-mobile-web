1edab4e0ffbeeccb78e0b8cdabd1676539448454
Simone Risola <elrisolix.erx@outlook.com>
2026-09-21 20:22:42 +0200

    implementazione targhetta andamento ritardo e miglioramenti UI nei dettagli treno

--- main.dart.js_1.part.js
+++ main.dart.js_1.part.js
@@ -1,7 +1,7 @@
 ((a,b)=>{a[b]=a[b]||{}})(self,"$__dart_deferred_initializers__")
 $__dart_deferred_initializers__.current=function(a,b,c,$){var A,B={
-bRi(){return new A.uH(null)}}
+bRp(){return new A.uH(null)}}
 A=c[0]
 B=a.updateHolder(c[3],B)
 var z=a.updateTypes([])};
-(a=>{a["4WsJz4gC+Z99ns8GFmwYWS9R1F8="]=a.current})($__dart_deferred_initializers__);
+(a=>{a["vpMi3DHntHfcM4v5bMM82kOGjGc="]=a.current})($__dart_deferred_initializers__);
