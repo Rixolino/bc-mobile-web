2c505e1b7649ea6705498d4a948d920a6ed8cbce
Simone Risola <elrisolix.erx@outlook.com>
2026-09-22 14:54:23 +0200

    fix(app): spettatori live, sezioni treni, ritardi regionali e targhetta andamento

--- main.dart.js_1.part.js
+++ main.dart.js_1.part.js
@@ -1,7 +1,7 @@
 ((a,b)=>{a[b]=a[b]||{}})(self,"$__dart_deferred_initializers__")
 $__dart_deferred_initializers__.current=function(a,b,c,$){var A,B={
-bRp(){return new A.uH(null)}}
+bRK(){return new A.uJ(null)}}
 A=c[0]
 B=a.updateHolder(c[3],B)
 var z=a.updateTypes([])};
-(a=>{a["vpMi3DHntHfcM4v5bMM82kOGjGc="]=a.current})($__dart_deferred_initializers__);
+(a=>{a["Cn/JBq9MLBOnAzTAHycxvCH2kWM="]=a.current})($__dart_deferred_initializers__);
