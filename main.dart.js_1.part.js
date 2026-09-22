2264789c55c8c2690bd89c0eb9435cf82d08492d
Simone Risola <elrisolix.erx@outlook.com>
2026-09-22 17:11:44 +0200

    fix(app): leave ritardati anti-fantasmi spettatori all'uscita dettaglio

--- main.dart.js_1.part.js
+++ main.dart.js_1.part.js
@@ -1,7 +1,7 @@
 ((a,b)=>{a[b]=a[b]||{}})(self,"$__dart_deferred_initializers__")
 $__dart_deferred_initializers__.current=function(a,b,c,$){var A,B={
-bRR(){return new A.uJ(null)}}
+bRY(){return new A.uK(null)}}
 A=c[0]
 B=a.updateHolder(c[3],B)
 var z=a.updateTypes([])};
-(a=>{a["4OcOx3BYm2W+qlCW/renxrMhbbM="]=a.current})($__dart_deferred_initializers__);
+(a=>{a["2uFiJYe/ju4alkLbmvS5fyL9ad4="]=a.current})($__dart_deferred_initializers__);
