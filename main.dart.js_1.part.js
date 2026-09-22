351208275bcb93efa1dfec3a7a04b0bdb1100345
Simone Risola <elrisolix.erx@outlook.com>
2026-09-22 15:23:22 +0200

    fix(app): ritardi regionali da tabellone, routing sheet regionale e orologio TV

--- main.dart.js_1.part.js
+++ main.dart.js_1.part.js
@@ -1,7 +1,7 @@
 ((a,b)=>{a[b]=a[b]||{}})(self,"$__dart_deferred_initializers__")
 $__dart_deferred_initializers__.current=function(a,b,c,$){var A,B={
-bRK(){return new A.uJ(null)}}
+bRM(){return new A.uJ(null)}}
 A=c[0]
 B=a.updateHolder(c[3],B)
 var z=a.updateTypes([])};
-(a=>{a["Cn/JBq9MLBOnAzTAHycxvCH2kWM="]=a.current})($__dart_deferred_initializers__);
+(a=>{a["bCd4zFhV2yr/YK3p2Z7oZzfj8Bw="]=a.current})($__dart_deferred_initializers__);
