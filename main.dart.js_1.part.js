ba446ebd48f18572e219ca16cb474fcca0913e49
Simone Risola <71252421+Rixolino@users.noreply.github.com>
2026-09-23 19:12:12 +0200

    annunci realistici, fix ritardo/progresso/mappa e landscape  - Annunci tabellone con frasi ufficiali MAS/NE/SNCF/DB e lettura numeri MAS - Master Annunci Realistici + tabellone/in-treno indipendenti - Onboard: elenco fermate e avviso siamo in arrivo a - Isteresi doppia conferma sul ritardo (nazionale+regionale) - Progresso robusto post-tabellone e con ritardo - Fix stile mappa non applicato - Preferiti esclusi dal landscape

--- main.dart.js_1.part.js
+++ main.dart.js_1.part.js
@@ -1,7 +1,7 @@
 ((a,b)=>{a[b]=a[b]||{}})(self,"$__dart_deferred_initializers__")
 $__dart_deferred_initializers__.current=function(a,b,c,$){var A,B={
-bRY(){return new A.uK(null)}}
+bVa(){return new A.uW(null)}}
 A=c[0]
 B=a.updateHolder(c[3],B)
 var z=a.updateTypes([])};
-(a=>{a["2uFiJYe/ju4alkLbmvS5fyL9ad4="]=a.current})($__dart_deferred_initializers__);
+(a=>{a["VzgnH7nQJ7Y4HY6kqFKtPYYBdEQ="]=a.current})($__dart_deferred_initializers__);
