aea283ee05c6b612ac8298b52cf9036c1e7e0d2d
Simone Risola <71252421+Rixolino@users.noreply.github.com>
2026-09-24 19:32:42 +0200

    major patcch

--- main.dart.js_1.part.js
+++ main.dart.js_1.part.js
@@ -1,7 +1,7 @@
 ((a,b)=>{a[b]=a[b]||{}})(self,"$__dart_deferred_initializers__")
 $__dart_deferred_initializers__.current=function(a,b,c,$){var A,B={
-bVa(){return new A.uW(null)}}
+bWx(){return new A.v2(null)}}
 A=c[0]
 B=a.updateHolder(c[3],B)
 var z=a.updateTypes([])};
-(a=>{a["VzgnH7nQJ7Y4HY6kqFKtPYYBdEQ="]=a.current})($__dart_deferred_initializers__);
+(a=>{a["dIb3GV2F6lk1CU+nY2r9T6TuLsA="]=a.current})($__dart_deferred_initializers__);
