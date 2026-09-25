2bc99a18107d9b66f5a34a13ff00374f11124008
Simone Risola <elrisolix.erx@outlook.com>
2026-09-25 12:14:03 +0200

    aggiornamento build web

--- main.dart.js_1.part.js
+++ main.dart.js_1.part.js
@@ -1,7 +1,7 @@
 ((a,b)=>{a[b]=a[b]||{}})(self,"$__dart_deferred_initializers__")
 $__dart_deferred_initializers__.current=function(a,b,c,$){var A,B={
-bWx(){return new A.v2(null)}}
+bWn(){return new A.v_(null)}}
 A=c[0]
 B=a.updateHolder(c[3],B)
 var z=a.updateTypes([])};
-(a=>{a["dIb3GV2F6lk1CU+nY2r9T6TuLsA="]=a.current})($__dart_deferred_initializers__);
+(a=>{a["xOmEZK6E6JHx3QbRf+NE+21Lwgc="]=a.current})($__dart_deferred_initializers__);
